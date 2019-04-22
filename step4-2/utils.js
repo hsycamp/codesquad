6f8a5e571561f5868cd4bf42ddbb38553e694c55
hsycamp <hsycamp@gmail.com>
2019-04-22 21:09:24 +0900

    예외처리 기능 구현

--- step4-2/utils.js
+++ step4-2/utils.js
@@ -1,17 +1,37 @@
 const isValidId = function (idNum) {
-    
+    if (idNum) {
+        return true;
+    } else {
+        console.log("id는 5자리 숫자여야합니다.");
+        return false;
+    }
 };
 
 const isValidStatus = function (status) {
-
+    if (status) {
+        return true;
+    } else {
+        console.log("상태는 todo, doing, done 중 하나로 입력해주세요.");
+        return false;
+    }
 };
 
 const isValidName = function (name) {
-    
+    if (name) {
+        return true;
+    } else {
+        console.log("name에는 숫자를 넣을 수 없습니다.");
+        return false;
+    }
 };
 
 const isValidTag = function (tag) {
-    
+    if (tag) {
+        return true;
+    } else {
+        console.log('tag를 올바른 형식으로 입력해주세요.');
+        return false;
+    }
 };
 
 const ranNum = function (digits) {
