f00084b008d5d524942ef12fd213d384070c65ca
hsycamp <hsycamp@gmail.com>
2019-04-26 21:57:42 +0900

    fix : person 배열의 index 계산식 수정
    person 배열의 반복 주기 계산 잘못함

--- algorithm/programmers/Level_1/ExhaustiveSearch.py
+++ algorithm/programmers/Level_1/ExhaustiveSearch.py
@@ -1,4 +1,4 @@
-# solution #1 //시간초과
+# solution #1
 def solution(answers):
     person1 = [1, 2, 3, 4, 5]
     person2 = [2, 1, 2, 3, 2, 4, 2, 5]
@@ -6,9 +6,9 @@ def solution(answers):
     count = [0, 0, 0]
     result = []
     for i in range(len(answers)):
-        j1 = i % (len(person1)+1)
-        j2 = i % (len(person2)+1)
-        j3 = i % (len(person3)+1)
+        j1 = i % len(person1)
+        j2 = i % len(person2)
+        j3 = i % len(person3)
         if(answers[i] == person1[j1]):
             count[0] += 1
         if(answers[i] == person2[j2]):
