5031f4f04cb640ea8388ac3d173ec80b7b070951
hsycamp <hsycamp@gmail.com>
2019-04-26 22:00:15 +0900

    feat : solution2 추가

--- algorithm/programmers/Level_1/ExhaustiveSearch.py
+++ algorithm/programmers/Level_1/ExhaustiveSearch.py
@@ -1,5 +1,5 @@
 # solution #1
-def solution(answers):
+def solution_1(answers):
     person1 = [1, 2, 3, 4, 5]
     person2 = [2, 1, 2, 3, 2, 4, 2, 5]
     person3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
@@ -17,5 +17,24 @@ def solution(answers):
             count[2] += 1
     for idx in range(len(count)):
         if(count[idx] == max(count)):
+            result.append(idx+1)
+    return result
+
+# solution #2
+def solution_2(answers):
+    person1 = [1, 2, 3, 4, 5]
+    person2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    person3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    count = [0, 0, 0]
+    result = []
+    for idx, answer in enumerate(answers):
+        if answer == person1[idx % len(person1)]:
+            count[0] += 1
+        if answer == person2[idx % len(person2)]:
+            count[1] += 1
+        if answer == person3[idx % len(person3)]:
+            count[2] += 1
+    for idx, c in enumerate(count):
+        if c == max(count):
             result.append(idx+1)
     return result
