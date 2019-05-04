ba98c33bab84a7accc0a27415e0253090eecee38
hsycamp <hsycamp@gmail.com>
2019-05-04 21:08:32 +0900

    Remove algorithm problems

--- algorithm/codility/PermMissingElem.py
+++
@@ -1,8 +0,0 @@
-'''
-# PermMissingElem
-Find the missing element in a given permutation.
-'''
-
-
-def solution(A):
-    return sum(range(1, len(A)+2))-sum(A)
--- algorithm/programmers/Level_1/ReturnMiddleCharacter.py
+++
@@ -1,7 +0,0 @@
-def solution(s):
-    answer = ''
-    if len(s) % 2 == 1:
-        answer = s[len(s)//2]
-    else:
-        answer = s[len(s)//2-1]+s[len(s)//2]
-    return answer
