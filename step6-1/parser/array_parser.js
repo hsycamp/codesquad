c862a5f5e2b52fd21da7d9cf248dc88a97908622
hsycamp <hsycamp@gmail.com>
2019-05-13 20:59:41 +0900

    feat : ArrayParser class 메소드 작성
    ArrayParser.arrayParse 메소드 작성
    ArrayParser.startParsing 메소드 작성

--- step6-1/parser/array_parser.js
+++ step6-1/parser/array_parser.js
@@ -27,6 +27,26 @@ class ArrayParser {
   constructor(tokenizer, lexer) {
     this.tokenizer = tokenizer;
     this.lexer = lexer;
+    this.queue = [];
+  }
+
+  arrayParse(parentNode) {
+    while (this.queue.length) {
+      const currentNode = this.queue.shift();
+      let lexedCurrentNode = this.lexer.lex(currentNode);
+      if (lexedCurrentNode.type === "endOfArray") return parentNode;
+      if (lexedCurrentNode.type === "array") {
+        lexedCurrentNode = this.arrayParse(lexedCurrentNode);
+      }
+      parentNode.child.push(lexedCurrentNode);
+    }
+  }
+
+  startParsing(str) {
+    this.queue = tokenizer.tokenize(str);
+    const rootNode = this.queue.shift();
+    const lexedRootNode = this.lexer.lex(rootNode);
+    return this.arrayParse(lexedRootNode);
   }
 }
 
@@ -35,5 +55,5 @@ const lexer = new Lexer();
 const arrayParser = new ArrayParser(tokenizer, lexer);
 
 const str = "[123, 22, 33]";
-// const result = arrayParser.arrayParse(str);
-// console.log(JSON.stringify(result, null, 2));
+const result = arrayParser.startParsing(str);
+console.log(JSON.stringify(result, null, 2));
